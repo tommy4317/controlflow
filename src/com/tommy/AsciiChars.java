e13235a1634ab9f54f26ec34bbecbc3fd14ab9dc
Tommy Mendez <tommymendez60@yahoo.com>
2021-06-28 16:21:19 -0500

    control flow examples

---
+++ src/com/tommy/AsciiChars.java
@@ -0,0 +1,25 @@
+package com.tommy;
+
+//This class will be called from Main
+
+public class AsciiChars {
+    public static void printNumbers() {
+        for (int i = 48; i <= 57; i++)
+            System.out.printf("%d : %c \n", i, i);
+
+    }
+
+    public static void printUpperCaseLetters(){
+
+        for (int i = 65; i <= 90; i++)
+            System.out.printf("%d : %c \n", i, i);
+
+    }
+
+    public static void printLowerCaseLetters(){
+
+        for (int i = 97; i <= 122; i++)
+            System.out.printf("%d : %c \n", i, i);
+
+    }
+}
